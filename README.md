6cf6f89d30278994ca53c789b5937d81846ab614
ClaudiuTertiu <claudiutertiu@gmail.com>
2021-11-25 14:36:58 +0200

    Later update

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# configcat-demo
+# ConfigCat Demo Java SDK
+
+##### Feature created for 2 users with identifier "romania-user" & "uk-user".
+##### Pentru "romania-user" - este setat pe false.
+##### Pentru "uk-user" - este setat pe true. 
+
+###### Please run the DemoApplication class.
+
+Examples of REST Api requests:
+
+ * GET http://localhost:8080/feature
+ * GET http://localhost:8080/feature?country=uk
+ * GET http://localhost:8080/feature?country=romania
--- src/main/java/com/configcat/demo/controllers/FeatureController.java
+++ src/main/java/com/configcat/demo/controllers/FeatureController.java
@@ -2,17 +2,17 @@ package com.configcat.demo.controllers;
 
 import com.configcat.ConfigCatClient;
 import com.configcat.PollingModes;
+import com.configcat.User;
 import com.configcat.demo.utils.ApplicationConstants;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FeatureController {
     @GetMapping("/feature")
-    public String testFeature() {
-        Logger logger = LoggerFactory.getLogger(FeatureController.class);
+    public String testFeature(@RequestParam(value = "country", required = false) String country) {
+        if(country == null) return "Romania or UK?";
 
         ConfigCatClient client = ConfigCatClient.newBuilder()
                 .mode(PollingModes.LazyLoad(
@@ -21,12 +21,27 @@ public class FeatureController {
                         )
                 )
                 .build(ApplicationConstants.CONFIG_CAT_KEY);
-        boolean isMyAwesomeFeatureEnabled = client.getValue(Boolean.class, "isMyFirstFeatureEnabled", false);
 
-        if(isMyAwesomeFeatureEnabled) {
-            return "You were lucky.";
-        } else {
-            return "Bad luck, try again.";
+        if(country.equals("romania")) {
+            User userObject = User.newBuilder()
+                .build("romania-user");
+                boolean isFromRomania = client.getValue(Boolean.class, "testFeature", userObject,false);
+                if(isFromRomania) return "Ce faci?";
+                else {
+                    return "Salut, feature-ul este disabled.";
+                }
         }
+        else if (country.equals("uk")) {
+            User userObject2 = User.newBuilder()
+                    .build("uk-user");
+            boolean isFromUk = client.getValue(Boolean.class, "testFeature", userObject2,false);
+            if(isFromUk) {
+                return "What do you do?";
+            }
+            else {
+                return "Hello, the feature is disabled";
+            }
+        }
+        return null;
     }
 }
